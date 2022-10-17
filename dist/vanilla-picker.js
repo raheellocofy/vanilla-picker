77217e6edcc78422d1f84210938e73d5e8f23cf8
Raheel <raheel@locofy.ai>
2022-10-17 13:07:43 +0500

    adds console

--- dist/vanilla-picker.js
+++ dist/vanilla-picker.js
@@ -726,7 +726,7 @@
                   return toggled;
               }
 
-              var html = this.settings.template || '<div class="picker_wrapper" tabindex="-1"><div class="picker_arrow"></div><div class="picker_hue picker_slider"><div class="picker_selector"></div></div><div class="picker_sl"><div class="picker_selector"></div></div><div class="picker_alpha picker_slider"><div class="picker_selector"></div></div><div class="picker_editor"><input aria-label="Type a color name or hex value"/></div></div>';
+              var html = this.settings.template || '<div class="picker_wrapper" tabindex="-1"><div class="picker_arrow"></div><div class="picker_hue picker_slider"><div class="picker_selector"></div></div><div class="picker_sl"><div class="picker_selector"></div></div><div class="picker_alpha picker_slider"><div class="picker_selector"></div></div><div class="picker_editor"><input aria-label="Type a color name or hex value"/></div><div class="picker_sample"></div><div class="picker_done"><button>Ok</button></div><div class="picker_cancel"><button>Cancel</button></div></div>';
               var wrapper = parseHTML(html);
 
               this.domElement = wrapper;
